cb5d2b147764f5888f8cbda95363046aa49f31e4
SHIKHAR <satyamshikhar@gmail.com>
2024-11-09 16:25:33 +0530

    changed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Define the home route
 @app.route('/')
 def hello_world():
-    return 'Hello, World! I am a ci-cd app'
+    return 'Hello, World! I am a ci-cd app - just added cd and runner'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
